f59c78a5da134ffc968489fec08f42b279d7c10d
Feederkyota <kyoooooooooota>
2024-07-01 22:32:01 +0900

    support for 5ch.net

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { AnchorEmbed } from '~/utils/AnchorEmbed';
-import imgurRule from '~/rules/imgur';
-import feederJumpPageResolver from '~/resolvers/feeder-jump-page';
-import youtubeRule from '~/rules/youtube';
-import nicovideoRule from '~/rules/nicovideo';
-import twitterRule from '~/rules/twitter';
-import xToTwitterResolver from '~/resolvers/x-to-twitter';
-import facebookRule from '~/rules/facebook';
-import instagramRule from '~/rules/instagram';
-import pinterestRule from '~/rules/pinterest';
+import { AnchorEmbed } from "~/utils/AnchorEmbed";
+import imgurRule from "~/rules/imgur";
+import feederJumpPageResolver from "~/resolvers/feeder-jump-page";
+import net5chJumpPageResolver from "~/resolvers/net5ch-feeder-jump-page";
+import youtubeRule from "~/rules/youtube";
+import nicovideoRule from "~/rules/nicovideo";
+import twitterRule from "~/rules/twitter";
+import xToTwitterResolver from "~/resolvers/x-to-twitter";
+import facebookRule from "~/rules/facebook";
+import instagramRule from "~/rules/instagram";
+import pinterestRule from "~/rules/pinterest";
 
 const anchorEmbed = new AnchorEmbed({
   rules: [
@@ -17,12 +18,13 @@ const anchorEmbed = new AnchorEmbed({
     twitterRule(),
     facebookRule(),
     instagramRule(),
-    pinterestRule()
+    pinterestRule(),
   ],
   resolvers: [
     feederJumpPageResolver(),
-    xToTwitterResolver()
-  ]
+    net5chJumpPageResolver(),
+    xToTwitterResolver(),
+  ],
 });
 
 anchorEmbed.apply();
